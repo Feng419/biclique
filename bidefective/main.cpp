71c78832a94c187ab5114606c41d0f1aa99c1eae
Charon <41113306+Feng419@users.noreply.github.com>
2024-04-08 23:32:55 +0800

    Update main.cpp

--- bidefective/main.cpp
+++ bidefective/main.cpp
@@ -1,12 +1,13 @@
 #include <string>
 #include <chrono>
 #include "bidefective.h"
-#include "getArgs.hpp"
+#include "../tools/getArgs.hpp"
 
 std::string file;
 int ls, rs, s, n;
 
 int main(int argc, char* argv[]) {
+    freopen("output1.txt", "w", stdout);
     argsController ac(argc, argv);
 
     if(!ac.exist("-f")) {
@@ -14,13 +15,15 @@ int main(int argc, char* argv[]) {
         return 0;
     }
     if(!ac.exist("-d")) {
-        printf("graph order: -d(core, two)\n");
+        printf("graph order: -d(core, no, deg)\n");
         return 0;
     }
 
-    uint32_t ls = 1, rs = 1;
+    int ls = 1, rs = 1, size = 0, total = 0;
     if(ac.exist("-l")) ls = std::stoi(ac["-l"]);
     if(ac.exist("-r")) rs = std::stoi(ac["-r"]);
+    if(ac.exist("-s")) size = std::stoi(ac["-s"]);
+    if(ac.exist("-t")) total = std::stoi(ac["-t"]);
 
     std::string fPath = ac["-f"];
     std::string order = ac["-d"];
@@ -35,19 +38,21 @@ int main(int argc, char* argv[]) {
     std::cout << "file path: " << fPath << " "
               << "k: " << k << " "
               << "order: " << order << " "
-              << "core" << core << " "
+              << "core: " << core << " "
               << "pivot: " << pivot << " "
               << "l : " << ls << " "
               << "r : " << rs << " "
+              << "size: " << size << " "
+              << "total: " << total << " "
               << "mode: " << graphMode << std::endl;
 
-    bidefective bic(fPath, k, ls, rs, order, core, pivot);
+    bidefective bic(fPath, k, ls, rs, order, core, pivot, size, total);
 
     auto t1 = std::chrono::steady_clock::now();      
     bic.run();
     auto t2 = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
     std::cout << "time:" << duration.count() << "ms" << std::endl;
-    
+    fclose(stdout);
     return 0;
 }  
